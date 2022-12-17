d51b3d80e82e3fd4fdf4cfdac8bacd76ef0ee048
enuiii <karpichev.vlad@mail.ru>
2022-12-17 08:56:01 +0200

    added lab_8

---
+++ Лабораторная №8/lab_8.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	short x;
+	short* p;
+	p = &x;
+
+	std::cout << "Введите любое целое число: ";
+	std::cin >> x;
+	std::cout << std::endl;
+
+	std::cout << p << std::endl;
+
+	for (int i = 0; i < sizeof(short); i++)
+	{
+		printf("%hhu ", *(((char*)&p) + i));
+	}
+}
