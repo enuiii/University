d088b2a45e0a872b1c8a9754e191280f6eaf0df3
enuiii <karpichev.vlad@mail.ru>
2022-12-17 09:01:12 +0200

    added lab_9

---
+++ Лабораторная №9/lab_9.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+// Класс
+class Square
+{
+	// Абстракция
+private:
+	double side_;
+
+	// Инкапсуляция
+public:
+	Square(double side) : side_(side)
+	{}
+
+	// Функция для нахождения диагонали
+	double get_diagonal()
+	{
+		return sqrt(2) * side_;
+	}
+};
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int n = 0;
+
+	std::cout << "Введите сторону квадрата: "; std::cin >> n; std::cout << std::endl;
+	Square square1(n);
+	std::cout << "Диагональ квадрата равна: " << square1.get_diagonal(); std::cout << std::endl;
+}
